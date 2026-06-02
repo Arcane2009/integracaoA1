cfc0f80090e348daaea83458acc025a672ca3b97
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2026-06-02 08:13:19 -0300

    Add files via upload

---
+++ integracaoMoraes-main/app.py
@@ -0,0 +1,5 @@
+#Import do framework e do render_template para leitura do HTML
+#Request para captura dos dados
+from flask import Flask, render_template, request
+#Biblioteca para criar uma conexão com o mysql
+import mysql.connector 
